f1c134a1c57a2e7170709456771f3ef529c5085a
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2022-08-27 11:20:29 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Navbar';
+export { default as ChannelDetail } from './Navbar';
+export { default as SearchFeed } from './Navbar';
+export { default as VideoDetail } from './Navbar';
+
